f798a45e52697ca8ab4f75615ee6709719a173e3
iagotero12 <iagoplorenzo@gmail.com>
2018-05-14 13:46:44 +0200

    serverwebnode

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');         //copiado de https://gist.github.com/gabmontes/e496a41f835bca65e99b
+
+var app = express();
+
+app.use(express.static('www'));
+
+var server = app.listen(80, function () {
+  console.log('Servidor web iniciado');
+});
