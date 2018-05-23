068ca2c28690661a10f4a245944fa0d1b7ce25fc
iagotero12 <iagoplorenzo@gmail.com>
2018-05-23 14:00:40 +0200

    finalizado sin estilo terminado

--- server.js
+++ server.js
@@ -1,20 +1,132 @@
-var express = require('express');         //copiado de https://gist.github.com/gabmontes/e496a41f835bca65e99b
-var bodyParser = require("body-parser")     //copiado de https://github.com/expressjs/body-parser
-var app = express();
-// create application/json parser
-var jsonParser = bodyParser.json()
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-var mysql = require('mysql')
-var fs = require("fs")
+var express = require('express');                                          //copiado de https://gist.github.com/gabmontes/e496a41f835bca65e99b
+var bodyParser = require("body-parser");
+var mysql = require('mysql');
+var fs = require("fs");
+var app = express();                                                      //copiado de https://github.com/expressjs/body-parser
+var jsonParser = bodyParser.json()                                        // create application/json parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })         // create application/x-www-form-urlencoded parser
+app.use(jsonParser);
+app.use(urlencodedParser);
 var listaTareas = [];
-var conection = mysql.createConnection({
+var connection = mysql.createConnection({                                 //Crear una conexion en la base de datos
   host: 'localhost',
   user: 'master',
   password: '1234',
   database: 'tareas db',
   port: 3306
 })
+
+connection.connect(function (err) {
+  if (err) {
+    console.log("Error al conectarse")
+  }
+  else {
+    console.log("Conexión correcta")
+  };
+});
+
+app.use(express.static('www/Tareas'));                                    //Convierte la página del server en la de la dirección www/Tareas
+app.get("/", function (req, res) {
+
+  connection.query('SELECT * FROM tareas', function (error, resultadoConsulta) {
+    if (error) {
+      throw error;
+    }
+    else {
+      console.log(resultadoConsulta);
+      fs.readFile("./www/Tareas/tarea.html", "utf8", function (err, text) {
+        var fila = cargarTareas(resultadoConsulta);
+        text = text.replace("[sustituir]", fila);
+        res.send(text);
+      });
+    }
+  });
+});
+app.get("/eliminar/:id?", function (req, res) {
+
+  console.log("Fichero de datos actualizado" + req.query.id);
+  connection.query("DELETE from tareas WHERE ID = ?", [req.query.id], function (err, resultadoConsulta) { //ID de base de datos, id de formulario
+    console.log("Record Deleted!!");
+    res.redirect("/");
+  });
+});
+app.post('/', function (req, res) {                                      //recibir peticiones en /datos usando method=get (debe coincidir con el method del index.html de tareas)G
+  console.log("petición recibida");
+  var nom = req.body.nombre || "";
+  var tar = req.body.tarea || "";
+  var nuevaTarea = { nombre: nom, tarea: tar }
+  actualizarBBDD(nuevaTarea);                                            //llama a la función actualizarBBD
+  fs.writeFile("tareas.json", JSON.stringify(listaTareas), function () {
+    console.log("Fichero de datos actualizado");
+  });
+  res.redirect("/")
+});
+app.get("/editar/:id?", function (req, res) {
+
+  connection.query("Select * from tareas", function (err, resultadoConsulta) { //ID de base de datos, id de formulario
+
+    console.log("resultadoConsulta");
+    var registroEditar;
+    for (const tarea of resultadoConsulta) {
+      if (tarea.ID == req.query.id) {
+        registroEditar = tarea;
+      }
+    }
+
+    fs.readFile("./www/Tareas/tarea.html", "utf8", function (err, text) {
+      var fila = cargarTareas(resultadoConsulta);
+      var nombre = registroEditar.Nombre;
+      var tarea = registroEditar.Tarea;
+
+      text = text.replace("[sustituir]", fila);
+      text = text.replace('action="/"', 'action="/editar"');
+      text = text.replace("[id_tarea]", req.query.id);
+      text = text.replace('placeholder="Nombres del usuario"', 'value="' + nombre + '"');
+      text = text.replace('placeholder="Nombre de la tarea"', 'value="' + tarea + '"');
+      res.send(text);
+    });
+  });
+});
+app.post("/editar", function (req, res) {
+  var nom = req.body.nombre || "";
+  var tar = req.body.tarea || "";
+  var id = req.body.id;
+  console.log(nom,tar,id);
+  connection.query("UPDATE tareas SET Nombre = ?, Tarea = ? WHERE ID = ?", [nom, tar, id], function (error, resultadoConsulta) {
+    //conectarse con base de datos y actualizar los datos
+    if (error) {
+      console.log(error);
+    } else {
+
+      console.log(resultadoConsulta);
+      res.redirect("/");
+    }
+  });
+});
+app.get("/pruebas", function (req, res) {
+  var nombre = "Pedro";
+  connection.query('SELECT * FROM tareas', function (error, result) {
+    if (error) {
+      throw error;
+    } else {
+      console.log(result);
+      nombre = result[0].Nombre;
+      var registros = cargarTareas(result);
+      console.log(registros)
+      fs.readFile("./www/Tareas/tarea.html", "utf8", function (err, text) {
+        text = text.replace("sustituir]", registros)
+        // res.send("Funciona ok, hola " + nombre);
+        res.send(text)
+      });
+
+    };
+
+  });
+});
+
+var server = app.listen(8080, function () {    //arranca servidor (puerto 8080)
+  console.log('Servidor web iniciado');
+});
 
 fileExists("tareas.json", (err, exists) => {
   if (err) {
@@ -31,46 +143,8 @@ fileExists("tareas.json", (err, exists) => {
   }
 });
 
-app.use(jsonParser);
-app.use(urlencodedParser);
-
-app.use(express.static('www/Tareas'));
-
-app.get("/", function (req, res) {
-  fs.readFile("./www/Tareas/tarea.html", "utf8", function (err, text) {
-    var fila = cargarTareas(listaTareas);
-    text = text.replace("[sustituir]", fila);
-    res.send(text);
-  });
-});
-
-app.get("/eliminar/:id?", function (req, res) {
-  listaTareas.splice(parseInt(req.query.id), 1);  //borrar un registro(req.body.nº del registro, cantidad de registro)
-  // function actualizar fichero(){
-  fs.writeFile("tareas.json", JSON.stringify(listaTareas), function () {
-    console.log("Fichero de datos actualizado");
-  });
-  res.redirect("/");
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function actualizarBBDD(datos) {
-  var query = conection.query('INSERT INTO tareas (nombre,tarea) VALUES(?,?)', [datos.nombre, datos.tarea],
+  var query = connection.query('INSERT INTO tareas (nombre,tarea) VALUES(?,?)', [datos.nombre, datos.tarea],
     function (error, result) {
       if (error) {
 
@@ -81,118 +155,26 @@ function actualizarBBDD(datos) {
       }
     });
 }
-
-var query = conection.query('SELECT * FROM tareas' ,function (error, result) {
-  if (error) {
-
-    throw error;
-  }
-  else {
-    console.log(result);
-
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.post('/', function (req, res) {       //recibir peticiones en /datos usando method=get (debe coincidir con el method del index.html de tareas)
-  console.log("petición recibida");
-  var nom = req.body.nombre || "";
-  var tar = req.body.tarea || "";
-  var nuevaTarea = { nombre: nom, tarea: tar }
-  listaTareas.push(nuevaTarea); //acumula los valores en un array para pintarlas todas en la web
-  // fs.writeFile("Prueba.txt","Hola, mundo")     //aunque no haya terminado de escribir, el programa sigue.
-  // // fs.writeFileSync ("Prueba.txt,"Hola mundo")    // //hasta que termina de escribir el programa no sigue.
-
-
-
-
-
-  actualizarBBDD(nuevaTarea);
-
-
-
-
-
-  fs.writeFile("tareas.json", JSON.stringify(listaTareas), function () {
-    console.log("Fichero de datos actualizado");
-  });
-  res.redirect("/")
-});
-
-var server = app.listen(8080, function () {    //arranca servidor (puerto 80)
-  console.log('Servidor web iniciado');
-});
-
-
-
-
-
 function cargarTareas(tareas) {
   var lista = "";
   for (var indice in tareas) {
     var fila = `
-<tr>
-<td>[id]</td>
-<td>[nombre]</td>
-<td>[tarea]</td>
-<td><a href="/eliminar?id=[id]">Eliminar</a>
-<a href="/editar?id=[id]">Editar</a></td>
-</tr>
-`;
+        <tr>
+        <td>[id]</td>
+        <td>[nombre]</td>
+        <td>[tarea]</td>
+        <td><a href="/eliminar?id=[id]">Eliminar</a>
+        <a href="/editar?id=[id]">Editar</a></td>
+        </tr>
+        `;
 
-    fila = fila.split("[id]").join(indice)
-    fila = fila.replace("[id]", indice)
-    fila = fila.replace("[nombre]", tareas[indice].nombre);
-    fila = fila.replace("[tarea]", tareas[indice].tarea);
+    fila = fila.split("[id]").join(tareas[indice].ID)
+    fila = fila.replace("[nombre]", tareas[indice].Nombre);
+    fila = fila.replace("[tarea]", tareas[indice].Tarea);
     lista += fila;
   }
   return lista;
 }
-
-
-app.get("/editar/:id?", function (req, res) {
-
-  fs.readFile("./www/Tareas/tarea.html", "utf8", function (err, text) {
-    var fila = cargarTareas(listaTareas);
-    var nombre = listaTareas[req.query.id].nombre;
-    var tarea = listaTareas[req.query.id].tarea;
-    text = text.replace("[sustituir]", fila);
-    text = text.replace('action="/"', 'action="/editar"');
-    text = text.replace("[id_tarea]", req.query.id);
-    text = text.replace('placeholder="Nombres del usuario"', 'value="' + nombre + '"');
-    text = text.replace('placeholder="Nombre de la tarea"', 'value="' + tarea + '"');
-    res.send(text);
-  });
-});
-
-app.post("/editar/:id?", function (req, res) {
-  var nom = req.body.nombre || "";
-  var tar = req.body.tarea || "";
-  var id = req.body.id;
-  listaTareas[id].tarea = tar;
-  listaTareas[id].nombre = nom;
-  fs.writeFile("tareas.json", JSON.stringify(listaTareas), function () {
-    console.log("Fichero de datos actualizado");
-  });
-  res.redirect('/');
-});
-
-
-
 function fileExists(file, cb) {
   fs.stat(file, (err, stats) => {
     if (err) {
@@ -204,32 +186,5 @@ function fileExists(file, cb) {
     }
     // devolvemos el resultado de `isFile`.
     return cb(null, stats.isFile());
-  });
-};
-
-
-
-
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'master',
-  password: '1234',
-  database: 'tareas db'
-});
-
-connection.connect(function (err) {
-  if (err) {
-    console.log("Error al conectarse")
-  }
-  else {
-    console.log("Conexión correcta")
-  };
-});
-
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
-
-connection.end();
+  })
+}
